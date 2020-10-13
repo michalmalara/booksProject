56a8670a836300a198ea2b2b1fce698f368a88b0
michalmalara <michalmalara@gmail.com>
2020-10-13 21:48:23 +0200

    Secret Key fixed

--- booksProject/settings.py
+++ booksProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = bool(os.environ.get("SECRET_KEY", False))
+SECRET_KEY = os.environ.get("SECRET_KEY", False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
