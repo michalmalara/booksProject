174f1831c9ceb61e6f1f91c23024163462ad02e4
michalmalara <michalmalara@gmail.com>
2020-10-13 23:18:42 +0200

    collectstatic fixed

--- books/urls.py
+++ books/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls.static import static
 
 from books import views
 
@@ -9,3 +10,5 @@ urlpatterns = [
     path('edit_book/<int:pk>', views.BookEditView.as_view(), name='book-edit'),
     path('delete_book/<int:pk>', views.BookDeleteView.as_view(), name='book-delete'),
 ]
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- booksProject/settings.py
+++ booksProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -205,5 +204,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-# django_heroku.settings(locals())
