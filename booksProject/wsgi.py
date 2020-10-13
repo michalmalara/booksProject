88d22db747885a1a2b8d44fb5e0483599a023043
michalmalara <michalmalara@gmail.com>
2020-10-13 23:52:12 +0200

    whitenoise wrapping in wsgi

--- booksProject/wsgi.py
+++ booksProject/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 
+from django.conf import settings
 from django.core.wsgi import get_wsgi_application
+
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booksProject.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.STATIC_ROOT, prefix='more-files/')
