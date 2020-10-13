4ae6ac2a854599bfaf2cf495ae1e85da4b526e3a
michalmalara <michalmalara@gmail.com>
2020-10-13 22:27:54 +0200

    whitenoise added

--- booksProject/settings.py
+++ booksProject/settings.py
@@ -81,6 +81,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'booksProject.urls'
