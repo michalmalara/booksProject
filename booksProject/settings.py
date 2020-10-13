4e268d456043382dc8d787f0aa3078538399d7c7
michalmalara <michalmalara@gmail.com>
2020-10-13 23:35:46 +0200

    staticfiles dir added

--- booksProject/settings.py
+++ booksProject/settings.py
@@ -203,6 +203,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
