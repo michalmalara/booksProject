236320a55bfdd8db0cc88faea05c2b0e4fd53307
michalmalara <michalmalara@gmail.com>
2020-10-14 00:01:57 +0200

    staticfiles collected

--- booksProject/settings.py
+++ booksProject/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", False)
+SECRET_KEY = 's9pv5^hyi#eos$3mo@ecf9rvy@x!t7c9=w9e93so#ju$83h*oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
